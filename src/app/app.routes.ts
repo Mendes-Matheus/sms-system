560e55471a6cb3ba3649cf4989d32993b4f2138e
Mendes-Matheus <msmmendes211@gmail.com>
2025-08-21 01:25:51 -0300

    update root components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Cadastro } from './pages/cadastro/cadastro';
+import { ListaPacientes } from './pages/lista-pacientes/lista-pacientes';
+import { Consultas } from './pages/consultas/consultas';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'cadastro', component: Cadastro },
+    { path: 'pacientes', component: ListaPacientes },
+    { path: 'consultas', component: Consultas }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
+
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ 
+      RouterOutlet,
+      RouterLink,
+      CommonModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
+
 export class App {
   protected readonly title = signal('sms-system-front');
+  menuOpen = false;
 }
